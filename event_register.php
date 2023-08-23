e20da9307f4d1c6339b7282a9720e19f3e028970
Rajmathan22 <mathanrajm2003@gmail.com>
2023-08-23 14:20:31 +0530

    title changed

--- event_register.php
+++ event_register.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Flair 2K19</title>
+    <title>Brahmastra 2k23</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
