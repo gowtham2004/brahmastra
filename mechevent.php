5c10f1468e7455d99450b863c4f7767632cddaf0
gowtham2004 <spideysundar2004@gmail.com>
2023-08-23 14:38:31 +0530

    mech title

--- mechevent.php
+++ mechevent.php
@@ -69,7 +69,7 @@ function closeNav() {
 <div id="cse" class="clearfix grid">
     <h3></h3><h3></h3><h3></h3>
     <?php if(isset($_SESSION['login_status'])){ ?> <h3 align="center" style="color: black" >Welcome <?php echo($_SESSION['login_user']); ?></h3> <?php } ?>
-    <h2 align="center" style="color: black">DEPARTMENT OF EEE</h2>
+    <h2 align="center" style="color: black">DEPARTMENT OF MECH</h2>
     <div class="figure-container">
       <figure class="effect-oscar wowload fadeInUp">
         <img src="images/portfolio/1.jpeg" height="300px" width="100%" alt="img01"/>
