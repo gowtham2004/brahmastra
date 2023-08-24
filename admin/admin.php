ff779acbcc084d56dd5463f2a71a89ba546ad80b
gowtham2004 <spideysundar2004@gmail.com>
2023-08-24 12:40:49 +0530

    A simple commit msg

--- admin/admin.php
+++ admin/admin.php
@@ -359,7 +359,6 @@
                     <!-- Content Row -->
 
                     <div class="row">
-
                         <!-- Area Chart -->
                         <div class="col-xl-8 col-lg-7">
                             <div class="card shadow mb-4">
@@ -558,85 +557,77 @@
                                 <div class="card-body">
                                     <!-- Styles -->
                                     <style>
-                                        #chartdiv1 {
+                                        #chart {
                                             width: 100%;
                                             height: 500px;
                                         }
                                     </style>
 
-                                <!-- Resources -->
-                                <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-                                <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-                                <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+                                    <!-- Resources -->
+                                    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+                                    <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+                                    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
 
-                                <!-- Chart code -->
-                                <script>
-                                    am5.ready(function() {
+                                    <!-- Chart code -->
+                                    <script>
+                                        am5.ready(function() {
 
-                                        // Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-var root = am5.Root.new("chartdiv1");
+                                            // Create root element
+                                            // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+                                            var root = am5.Root.new("chartdiv1");
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-root.setThemes([
-  am5themes_Animated.new(root)
-]);
+                                            // Set themes
+                                            // https://www.amcharts.com/docs/v5/concepts/themes/
+                                            root.setThemes([
+                                                am5themes_Animated.new(root)
+                                            ]);
 
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
-var chart = root.container.children.push(
-  am5percent.PieChart.new(root, {
-    endAngle: 270
-  })
-);
+                                            // Create chart
+                                            // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+                                            var chart = root.container.children.push(
+                                                am5percent.PieChart.new(root, {
+                                                    endAngle: 270
+                                                })
+                                            );
 
-// Create series
-// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
-var series = chart.series.push(
-  am5percent.PieSeries.new(root, {
-    valueField: "value",
-    categoryField: "category",
-    endAngle: 270
-  })
-);
+                                            // Create series
+                                            // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+                                            var series = chart.series.push(
+                                                am5percent.PieSeries.new(root, {
+                                                    valueField: "value",
+                                                    categoryField: "category",
+                                                    endAngle: 270
+                                                })
+                                            );
 
-series.states.create("hidden", {
-  endAngle: -90
-});
+                                            series.states.create("hidden", {
+                                                endAngle: -90
+                                            });
 
-// Set data
-// https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
-series.data.setAll([{
-  category: "Lithuania",
-  value: 501.9
-}, {
-  category: "Czechia",
-  value: 301.9
-}, {
-  category: "Ireland",
-  value: 201.1
-}, {
-  category: "Germany",
-  value: 165.8
-}, {
-  category: "Australia",
-  value: 139.9
-}, {
-  category: "Austria",
-  value: 128.3
-}, {
-  category: "UK",
-  value: 99
-}]);
+                                            // Set data
+                                            // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+                                            series.data.setAll([
+                                                {
+                                                    category: "Lithuania",
+                                                    value: 501.9
+                                                }, {
+                                                    category: "Czechia",
+                                                    value: 301.9
+                                                }, {
+                                                    category: "Ireland",
+                                                    value: 201.1
+                                                }, {
+                                                    category: "Germany",
+                                                    value: 165.8
+                                                }]);
 
-series.appear(1000, 100);
+                                            series.appear(1000, 100);
 
-}); // end am5.ready()
-</script>
+                                        }); // end am5.ready()
+                                    </script>
 
-<!-- HTML -->
-<div id="chart"></div>
+                                    <!-- HTML -->
+                                    <div id="chart"></div>
                                 </div>
                             </div>
                         </div>
