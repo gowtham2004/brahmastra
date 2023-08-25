3b0e198adfff922777ef87068304c1f8cf16b6c7
Giriraaj <127583991+Giriraaj2004@users.noreply.github.com>
2023-08-25 13:02:04 +0530

    eee rules updated

--- eeeevent.php
+++ eeeevent.php
@@ -76,7 +76,7 @@ function closeNav() {
             <img src="images/portfolio/1.jpeg" height="100%" width="100%" alt="img01"/>
             <figcaption>
                 <h2 style="color: #f5f2f2;">Current Xplore</h2>
-                <p><br>Time: 11AM-3PM<br>Venue: xxxxxxxx<br>
+                <p><br>Time: 10.30am-12.30pm<br>Venue: G-block seminar hall<br>
                     <br>
                 <a data-toggle="modal" data-target="#myeee1">View more</a></p>
             </figcaption>
@@ -87,7 +87,7 @@ function closeNav() {
             
             <figcaption><br><br><br><br><br>
                 <h2 style="color: #fafafa; font-size: 34px;">ElectriTech Expo</h2>
-                <p>Time: 1PM-3PM<br>Venue: xxxxxxx<br>
+                <p>Time: 10.30am-12.30pm<br>Venue: EEE Workshop<br>
                     <br>
                 <a data-toggle="modal" data-target="#myeee2">View more</a></p>
             </figcaption>
@@ -97,7 +97,7 @@ function closeNav() {
             <img src="images/portfolio/c3.jpeg" height="300px" width="100%" alt="img01"/>
             <figcaption>
                 <h2 style="color: #fffbfb;">Minute To Win It</h2>
-                <p><br>Time: 11AM-3PM<br>Venue: xxxxxxxx<br>
+                <p><br>Time: 2am<br>Venue: G block lobby<br>
                 <a data-toggle="modal" data-target="#myeee3">View more</a></p>
             </figcaption>
         </figure>
@@ -106,7 +106,7 @@ function closeNav() {
             <img src="images/portfolio/cc4.jpeg" height="300px" width="100%" alt="img01"/>
             <figcaption><br><br>
                 <h2 style="color: #111;">Buzzy</h2>
-                <p><br>Time: 9AM-11AM <br>Venue: xxxxxx<br>
+                <p><br>Time: 2am <br>Venue: G-block lobby<br>
                 <a data-toggle="modal" data-target="#myeee4">View more</a></p>
             </figcaption>
         </figure>
@@ -149,28 +149,47 @@ function closeNav() {
         <h4 class="modal-title">CURRENT XPLORE</h4>
       </div>
       <div class="modal-body">
-        <p>Are you a passionate Coder who likes to play with Algorithmic challenges,then sharpen your wit to face an exhilarating set of problems. Remember speed and accuracy matters a lot. Come up with a solution, code it and beat the clock to prove your prowess</p>
-		<br>
+        
 		
-		<p><b>FORMAT </b><br>
-		• It is a single round coding contest with six problems,spanning for 150 minutes<br>
-		• Problem Statement will be given such that the input to the program and the desired output are defined<br>
-		• Participants are expected to write the code that performs the necessary processing/transformations<br>
-		• A submitted solution is checked against several testcases and a response will be generated<br>
-		• The objective is to solve the as many problems as possible</p>
+		<p><b>Topics</b><br>
+
+    “Smart Grid solutions for Sustainable Energy Managemnet”<br>
+    “Advancements in Power Electronics and Renewable Energy”<br>
+    “ Innovative IoT Applications in Electrical Systems”<br>
+    “Electrifying the Future:Electric Vehicles and Infrastructure”<br>
+    “Emerging Trends in Robotics and Automation”<br>
+    “Power Quality and Grid Stability: Challenges and Solutions”<br>
+    “AI and Machine Learning for Energy Efficiency”<br>
+    “Energy Harvesting Technologies for Self-Sustainable Systems”<br>
+    “Smart Sensors for Real-Time Monitoring”<br>
+    “Intelligent Control Techniques for Power Systems”<br>
+    “Electric Power Systems for Smart Cities”<br>
+    “Applications of Blockchain in Energy Management”<br>
+    “Energy Storage Systems and Battery Technologies”<br>
+    “Wireless Power Transfer and Energy Harvesting”<br>
+		
 		
 		<p><b>RULES</b><br>
-		• Each team should have two members<br>
-		• The team members need not be from the same institution/college<br>
-		• Mobile phones and other materials are not allowed inside the contest arena<br>
-		• Teams involved in any kind of malpractice will be immediatly disqualified<br>
-		• The decision taken by the organizers will be final<br>
-		• Lone wolves are not allowed</p>
+		•The team size is maximum of 2 members<br>
+    •Each participant will be given 8 minutes for presentation and 2minutes for question session<br>
+    •Participate should present their idea within 15-20 slides<br>
+    •Candidates should send their paper in soft copy to our mail id(brahmastra23@chettinadtech.ac.in )and also 1 hard copy at the presenting time<br>
+    •Participants should send their paper before presenting on stage<br>
+    •Accepted papers will be intimated through their respective mail.id<br>
+    •Participants are requested to have a copy of their presentation their mail or in pen drive<br>
+    •Paper should be submitted as a MS word document (.doc) or pdf document<br>
+    •Name, Department, E-Mail ID, Contact number, Institution name should be sent along with the paper<br>
+		</p>
 		
-		<p><b>CONTACT DETAILS</b><br>
-		 Ram Prakash P : 9489889301<br>
-		 Sailesh       : 9092499086
-		 </p>
+		<p><b>Student Co-ordinators</b><br>
+    T.Priyadharshini-IV EEE (9994581021)<br>
+    E.Muniyappan-III EEE (8072159305)</pr>
+
+    <p><b>Staff Co-ordinator</b><br>
+    Dr.M.Senthilkumar , Professor/EEE<br></pr>
+
+    <p><b>Time & Venue</b><br>
+    10:30AM to 12:30 & G-Block Seminar Hall<br></p>
 		  <?php if(isset($_SESSION['login_status']) && !isset($_SESSION['event_status'])) { ?>
       <button type="button" style="background-color:green; " class="btn btn-default btn-block" onclick="window.location.href='event_register.php';">Register Now</button> 
     <?php } else if(isset($_SESSION['event_status'])) { ?>
@@ -184,6 +203,7 @@ function closeNav() {
 </div>
 
 
+
 <div id="myeee2" class="modal fade" role="dialog" >
   <div class="modal-dialog" >
 
@@ -191,30 +211,25 @@ function closeNav() {
     <div class="modal-content" >
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">ELECTRITECH EXPO</h4>
+        <h4 class="modal-title">ELECTRITECH EXPO(Showcase of Electrical Innovations)</h4>
       </div>
       <div class="modal-body">
-		<p>Are you someone who loves designing as well as coding? Have you sat hours at an end trying to make a website and loved it?</p>
-		This is Web ninja, a competition where you can showcase your talents in coding as<br> well as designing. Put your skills to the test and design a website as the clock ticks<br>
-		
-		<br><p><b>Round 1</b><br>
-		• There will be an MCQ in all the web technologies which will test your design skills<br>
-		• There will be 30 questions which has to be answered in 30 minutes</p>
-		
-		<p>Round 2</b><br>
-		• You will be given a template on spot, out of which you need to create the nearest match of it<br>
-		• The team who creates an exact or nearest match within a short period of time will be declared as winners</p>
 		
 		<p><b>RULES</b><br>
-		• A team must consists of 2 members<br>
-		• Team members can be from different colleges<br>
-		• Usage of internet, mobile phones and other electronic gadgets is strictly prohibited<br>
-		• Basic knowledge in HTML, CSS and JavaScript is needed<br>
-		• Decision of the Judges will be final</p>
+		• Maximum three students per team<br>
+    •Four minutes presentation and two minutes answering query<br>
+    •Winner list based on innovation and demonstrate the project<br>
+    •The arbitrator’s decision is final<br></p>
 		
-		<p><b>CONTACT DETAILS</b><br>
-		Vasanthan S : 9894753624<br>
-		Rashmi :9443314141</p>
+		<p><b>Student Co-ordinators</b><br>
+		P.Senthilmurugan-IV EEE (9361620179)<br>
+    S.Lakshan-III EEE (8124750515)</p>
+
+    <p><b>Staff Co-ordinator</b><br>
+    Ms.A.Bhuvaneswari, AP/EEE</p>
+
+    <p><b>Time & Venue</b><br>
+    EEE Workshop & 10.30AM to 12.30PM</p>
 
 		 <?php if(isset($_SESSION['login_status']) && !isset($_SESSION['event_status'])) { ?>
       <button type="button" style="background-color:green; " class="btn btn-default btn-block" onclick="window.location.href='event_register.php';">Register Now</button> 
@@ -234,42 +249,30 @@ function closeNav() {
     <div class="modal-content" >
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">MINUTE TO WIN IT</h4>
+        <h4 class="modal-title">MINUTE TO WIN IT(Suspense Event)</h4>
       </div>
       <div class="modal-body">
-		<p> Have you always been fascinated by the world of Android? Then brace yourselves for 'ANDROXUS 2k23'.</p>
-		<br> Here's your chance to prove that you're unbeatable in your app development skills</br>
-		<p><b>DESCRIPTION</b><br>
-		Are you someone who’s always got the eye for spotting errors? Then this round’s for you! It’s just a warm-up to the much awaited Android app development round! 
-		This will be a code debugging round, where you will be provided with a program with some errors in it.
-		You’ll have to debug the errors within the given time. 
-		Eclipse IDE will be used. Let’s see how you debug your way through the grand finals !</p>
-		
-		<p><br><b>Round 1</b></br>
-		• The first round will be a debugging round in which the participants will be debugging a program<br>
-	    • The selection will be based on the maximum number of errors found and least time taken to correct it<br>
-		• Eclipse IDE will be used for this round<br></p>
-		
-		<p><b>DESCRIPTION</b><br>
-		Winning this round is what it takes to be THE ANDROXUS. Can you become an ANDROXUS? Then show us how good your designing and implementing skills are in Android! 
-		It’s more than enough to have a basic understanding of Android, because, guess what, you’ll be given Internet access! 
-		This round will be an Android App Development round. 
-		In this, you’ll have to choose a slot and develop an Android app for the problem statement specified in that slot. 
-		Can you, with the help of Mr.Google, make your way to becoming the ANDROXUS of 2k23?
-		
-		<p><br><b>Round 2</b></br>
-		• This is the round where you will be exhibiting your android skills<br>
-		• You will be given with the problem statement for which you need to showcase your skills<br>
-		• The winners will be selected on the basis of UI/UX, performance, Memory Space usage<br></p>
 		
 		<p><br><b>RULES</b></br>
-		• Competitors must develop only native android apps<br>
-		• Competitors must bring their own android phones and connectors in case they need<br> 
-		• Participants must only work with android studio<br></p>
+		• Individual / Group participants<br>
+    • Fun games based on the Given Material<br>
+    • Maximum 4 rounds will be conducting<br></p>
 		
-		<p><br><b>CONTACT DETAILS</b></br>
-		Akil Ayyapan R A : 9787919223<br>
-        Krishna Prasad A S : 8608648420</p>
+		
+    
+    <p><b>Student Co-ordinators</b><br>
+		R.Kowsalya-IV EEE (9345785875)<br>
+    S.Karthickeyan-IV EEE (9345599244)<br>
+    T.Saran-III EEE (8610669610)</p>
+
+    <p><b>Staff Co-ordinator</b><br>
+    Ms.D.Pushpalatha, SAP/EEE</p>
+
+    <p><b>Time & Venue</b><br>
+    2PM & G-Block Lobby </p>
+
+    <p><b>NOTE</b><br>
+    Selected candidates should bring their identity card at the time of presentation<p>
 		 <?php if(isset($_SESSION['login_status']) && !isset($_SESSION['event_status'])) { ?>
       <button type="button" style="background-color:green; " class="btn btn-default btn-block" onclick="window.location.href='event_register.php';">Register Now</button> 
     <?php } else if(isset($_SESSION['event_status'])) { ?>
@@ -290,37 +293,35 @@ function closeNav() {
     <div class="modal-content" >
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">BUZZY</h4>
+        <h4 class="modal-title">BUZZY(Fun Events)</h4>
       </div>
       <div class="modal-body">
-        <p>Explore a new dimension of coding through <b>Carbine Coders 2k23!!</b> Quick-witted people with basic coding skills are the ones we’re looking for. 
-		The event will also test the swift thinking abilities and problem solving skills of the participants.</p>
+        <p>Tech Buzz is a Fun Event. A path of wire is there and a handle with small hole is given. Participants 
+have to move that handle along the path within 5minutes. If the Handle touched the path wire, a 
+buzzer will alarm. A person who completes the path without the alarm sound, they said to be 
+Winner</p>
 		
-		<p><b>Round 1: Get acquainted with Python</b><br>
-		• Have you always wanted to learn Python, but never got around to it? Take your first baby steps in Python with us. <br>
-		• You’ll be provided with a few programs in python, where the lines of code are jumbled.<br>
-		• Rearrange the lines of code to pass the test cases and stay alive in the competition.<br>
-		  <b>P.S. </b>You’ll have access to the Internet, in case you need to sort out your syntactical queries.<br>
-		  <br><b>Selection criteria:</b><br>
-		  The team that solves the most number of problems in the given 45 minutes will qualify for the next round.</p>
 		
-		<p><b>Round 2: A twist in the tale</b><br>
-		• Each team will be provided with two different problem statements,one for each member.<br>
-		  <b>P.S. </b> Expect the unexpected.<br>
-		  <br><b>Selection criteria:</b><br>
-		  The team that solves both the problems statements the fastest will be declared the winners.</p>
 		
 			
 		<p><b>RULES</b><br>
-		• Each team must consist of two members, not necessarily from the same college<br>
-		• Mobiles phones and other electronic gadgets are strictly prohibited<br>
-		• Decisions taken by the Judges will be final and binding</p><br>
+		• Only one participant is allowed to participate at a time<br>
+    • The Handle should not touch the path more than 3 times<br>
+    • If it touches more than 3 times the participant will be declared as out<br>
+    • A person who completes the path is declared as Winner</p><br>
 		
 		
-		
-		<p><b>Event Co-ordinators:</b><br>
-		 Hari Nandha J R : 9003791901<br>
-		 Rishika Jain A : 9487583082</p>
+
+     <p><b>Student Co-ordinators</b><br>
+      S.Pavithra-IV EEE (8610004603)<br>
+      S.Pranav-IV EEE (9344480066)<br>
+      R.Nikithalakshmi-IV EEE (7397724334) </p>
+
+    <p><b>Staff Co-ordinator</b><br>
+    Ms.D.Pushpalatha, SAP/EEE</p>
+
+    <p><b>Time & Venue</b><br>
+    2PM & G-Block Lobby</p>
 		 
 		  <?php if(isset($_SESSION['login_status']) && !isset($_SESSION['event_status'])) { ?>
       <button type="button" style="background-color:green; " class="btn btn-default btn-block" onclick="window.location.href='event_register.php';">Register Now</button> 
